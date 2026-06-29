e44aca2e3dfde4e116085737380807f1f7f01b30
rsodlulash <r.sodlulash@alustudent.com>
2026-06-29 07:35:13 -0700

    Functions-The oracles's vision

---
+++ quests/quest_23_The_Oracle_Vision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Testing out Functions
+def calculate_area(length, width):
+    return length * width
+#Call the function for two rectangles
+area1 = calculate_area(7, 3)
+area2 = calculate_area(11, 3)
+#Results
+print(f"The area of the first rectangle is {area1}.")
+print(f"The area of the second rectangle is {area2}.")
